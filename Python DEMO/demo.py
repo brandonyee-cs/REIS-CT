244c49bc8c521d9d46b0fa4d532c289bae3aa863
Brandon Yee <brandonyee.nyc@gmail.com>
2024-03-25 10:06:28 -0400

    Fixed syntax issue in properties.py 'UC' dictionary. Demo issue with UC dataframe still persists.

--- Python DEMO/demo.py
+++ Python DEMO/demo.py
@@ -3,16 +3,15 @@ from systems import timeline as tl
 
 print("Welcome to the Python DEMO for REIS-CT \nThis is DEMO was created as a submission for the CT LT Govs Computing Challenge.\nCreated by Brandon Yee, Rohak Gulia, and Shayaan Siddiqui.")
 
-#Setting Up Through Systems Start Functions
-
+#Initialization
 DEMO = sys()
-demo_usertype = sys.getuserType(DEMO)
 sys.balStart(DEMO)
-demo_balance = sys.getBalance(DEMO)
 
-print(f"Player Type: {demo_usertype} \nUC = Upper Class; MC = Middle Class; WC = Working Class")
-print(f"Balance: {demo_balance}")
+#Prints Basic Information
+print(f"Player Type: {sys.getuserType(DEMO)} \nUC = Upper Class; MC = Middle Class; WC = Working Class")
+print(f"Balance: {sys.getBalance(DEMO)}")
 
 print(sys.getProplist(DEMO))
-sys.getProp(DEMO)
-
+sys.buyProp(DEMO)
+sys.morgage(DEMO)
+print(sys.getBalance)
--- Python DEMO/systems.py
+++ Python DEMO/systems.py
@@ -1,63 +1,40 @@
-import random
-import pandas as pd
-import properties as prop
+import random; import pandas as pd; import properties as prop
 
 class systems:
     def __init__(self):
-        self.userType_array = ['WC', 'MC', 'UC'] #WC = Working Class | MC = Middle Class | UC = Upper Class
-        self.userType = random.choice(self.userType_array)  # WC, MC or UC
+        self.userType_array = ['WC', 'MC', 'UC']; self.userType = random.choice(self.userType_array) #WC = Working Class | MC = Middle Class | UC = Upper Class
         
     def getuserType(self):
         return self.userType
     
     def balStart(self):
-        if self.userType == 'WC':
-            self.balance = 50000 #Working Class Starting Balance
-        elif self.userType == 'MC':
-            self.balance = 500000 #Middle Class Starting Balance
-        elif self.userType == 'UC':
-            self.balance = 5000000 #Upper Class Starting Balance
-    
-    def getBalance(self):
-        return self.balance #Returns Balance | Visuals tbd
+        if self.userType == 'WC': self.balance = 50000
+        elif self.userType == 'MC': self.balance = 500000
+        elif self.userType == 'UC': self.balance = 5000000
     
     def getProplist(self):
-        if self.userType == 'WC':
-            self.df = pd.DataFrame(data=prop.wc_proplist)
-            self.dictvar = prop.wc_proplist
-        elif self.userType == 'MC':
-            self.df = pd.DataFrame(data=prop.mc_proplist)
-            self.dictvar = prop.mc_proplist
-        elif self.userType == 'UC':
-            self.df = pd.DataFrame(data=prop.uc_proplist)
-            self.dictvar = prop.uc_proplist
-        #df.index = ['Location:', 'Bed/Bath:', 'Est.Cost:', 'Morgage Cost:'] Fix Row Names
+        df = self.df
+        if self.userType == 'WC': self.df = pd.DataFrame(data=prop.wc_proplist); self.dictvar = prop.wc_proplist
+        elif self.userType == 'MC': self.df = pd.DataFrame(data=prop.mc_proplist); self.dictvar = prop.mc_proplist
+        elif self.userType == 'UC': self.df = pd.DataFrame(data=prop.uc_proplist); self.dictvar = prop.uc_proplist
         return self.df
     
-    def getProp(self):
-            validpropID = False
-            while validpropID != True:
-                propID = input("Enter the ID (First Row) of the property you want to purchase: ")
-                if propID in self.dictvar:
-                    validpropID = True
-                else:
-                    print("Invalid PropID; Try Again")
-            self.prop = self.df.get(propID)
-            return self.prop
-            
-                
-
     def morgage(self):
         #Mortgages a property
         propVal = self.prop[3]
-        if self.getBalance >= propVal:
-            self.balance -= propVal
-            return self.propInfo
-        elif self.getBalance >= propVal:
-            self.balance -= 0.06 * propVal
-        else:
-            self.propInfo == 'ERROR // PROPERTIES DISPLAYED INVALID \nReload to Reset'
-            return self.propInfo
+        if self.balance >= propVal: self.balance -= propVal
+        elif self.balance >= propVal: self.balance -= 0.06 * propVal
+        else: self.propInfo == 'ERROR // PROPERTIES DISPLAYED INVALID \nReload to Reset'
+
+    def buyProp(self):
+        validpropID = False
+        df = self.df
+        while validpropID == False: 
+            propID = int(input("Enter the ID (First Row) of the property you want to purchase: "))
+            self.prop = self.df.get(propID)
+            if self.prop is not None: break
+            else: print("Invalid PropID; Try Again")
+        self.prop = df.get(propID)
 
 class timeline(systems):
     def __init__(self, userType, prop) -> None:
